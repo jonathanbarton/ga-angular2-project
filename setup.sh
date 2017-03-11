e859434142f0ae5e2fa36b48e7f8f432199818e4
Jonathan Barton <jonathan.barton@mheducation.com>
2017-03-11 12:10:03 -0500

    initial commit - scaffolding and setup added

---
+++ setup.sh
@@ -0,0 +1,9 @@
+npm install -g n
+n 6.
+npm install -g npm@3.10.0
+npm install -g npm@next
+n 7.
+npm uninstall -g angular-cli
+npm cache clean
+npm install -g angular-cli@latest
+ng set --global warnings.packageDeprecation=false
