308506105e0244a8efd5e26f4acbeba981d4c99a
Jonathan Barton <jonathan.m.barton@gmail.com>
2017-03-16 12:11:17 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,9 +3,9 @@ n 6.
 npm install -g npm@3.10.0
 npm install -g npm@next
 n 7.
-npm uninstall -g angular-cli
+npm uninstall -g @angular/cli
 npm cache clean
-npm install -g angular-cli@latest
+npm install -g @angular/cli
 ng set --global warnings.packageDeprecation=false
 ng set defaults.styleExt scss
 npm install -g json-server
