da65c3937ba73f51b930ecd284ef49bf3ec498fe
ladypinetree <maureenpine@gmail.com>
2017-03-17 16:28:56 -0400

    Add in dispense item and balance service Worked with Andrew Goode (akgoode)  - Adds in fix for .5 and .10 add and deduct balance

---
+++ src/app/select-item/select-item.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { ItemService } from '../item/item.service';
+
+@Component({
+  selector: 'app-select-item',
+  templateUrl: './select-item.component.html',
+  styleUrls: ['./select-item.component.css']
+})
+export class SelectItemComponent implements OnInit {
+  public items;
+  public title = 'Lebron is not the GOAT';
+  constructor(public itemService: ItemService) { }
+
+  ngOnInit() {
+    this.itemService.onItemsRetrieved((items) => {
+      this.items = items;
+    })
+  }
+  onItemSelected(item) {
+    this.itemService.setSelectedItem(item);
+  }
+}
