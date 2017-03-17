088d9affc86490936944ad0e89ec49da8e368f27
Stephen Wisner <spwisner@gmail.com>
2017-03-17 15:19:34 -0400

    Done

---
+++ src/app/dispense-item/dispense-item.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dispense-item',
+  templateUrl: './dispense-item.component.html',
+  styleUrls: ['./dispense-item.component.css']
+})
+export class DispenseItemComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
